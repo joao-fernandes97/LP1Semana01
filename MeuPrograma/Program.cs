e3e8fc94fd6036cbbc67564a5bbe17e91594e685
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-21 20:23:57 +0000

    Add bye world

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace MyProgram
+namespace MeuPrograma
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye world");
         }
     }
 }
