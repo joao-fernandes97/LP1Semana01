9475bead780fc127189137faf2f1c41a946c3e4e
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-21 22:22:53 +0000

    Added more strings with concatenation and interpolation and a print to the console

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,9 +6,18 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
-            string s1 = "Um tab \t e um copyright\u03A9";
+            string s1 = "A tab \t and a copyright sign\u03A9";
             string s2 = "\"\\\'\n\'\"\\\"\'\"\"";
             Console.WriteLine($"{s1}\n{s2}");
+
+            string conc1 = "a"+2;
+            string conc2 = "abc"+s2;
+            string inter1 = $"value of conc1 is {conc1}";
+            int x = 5;
+            int y = 10;
+            string inter2 = $"{x} plus {y} equals {x+y}";
+            Console.WriteLine($"{conc1}\n{conc2}\n{inter1}\n{inter2}");
+
         }
     }
 }
