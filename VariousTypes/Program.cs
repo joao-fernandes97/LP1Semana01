b36b4e699fa7415179c92cdb7867191be357b7e8
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-21 21:17:33 +0000

    Added char variables and print to console

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 
 namespace VariousTypes
 {
@@ -6,11 +8,19 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding=Encoding.UTF8;
+            
             int n = 5;
             uint un = 34U;
             short s = 12; //couldn't find a "tag" for a short int
             long l = 77L;
             Console.WriteLine($"{n}\n{un}\n{s}\n{l}");
+
+            char triangle ='\u2C21';
+            char eye = '\u0f17';
+            char pog = '\u1435';
+            char five = '\u168e';
+            Console.WriteLine($"{triangle}\n{eye}\n{pog}\n{five}");
         }
     }
 }
