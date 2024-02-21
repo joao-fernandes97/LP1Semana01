fb3584af27d116fb788f487c76e259fdc458868b
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-21 21:01:20 +0000

    Added proper literal formatting

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -7,9 +7,9 @@ namespace VariousTypes
         static void Main(string[] args)
         {
             int n = 5;
-            uint un = 34;
-            short s = 12;
-            long l = 77;
+            uint un = 34U;
+            short s = 12; //couldn't find a "tag" for a short int
+            long l = 77L;
             Console.WriteLine($"{n}\n{un}\n{s}\n{l}");
         }
     }
