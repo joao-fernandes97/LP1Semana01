4e6a64234c919b68475eb007dc82300daef2c2a6
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-21 22:15:54 +0000

    Added string variables and print to console

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Um tab \t e um copyright\u03A9";
+            string s2 = "\"\\\'\n\'\"\\\"\'\"\"";
+            Console.WriteLine($"{s1}\n{s2}");
+        }
+    }
+}
