d793450b61ce37142c1f4705c7dae80ddb40ddff
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-21 20:57:56 +0000

    Added int variables and printed them to console

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            uint un = 34;
+            short s = 12;
+            long l = 77;
+            Console.WriteLine($"{n}\n{un}\n{s}\n{l}");
+        }
+    }
+}
