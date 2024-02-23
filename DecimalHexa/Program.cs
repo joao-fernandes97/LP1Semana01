f34434e597602a0221639a8935ad04efea3727ad
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-02-23 16:21:25 +0000

    Initial commit and asking for input

---
+++ DecimalHexa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DecimalHexa
+{
+    //ask 4 whole numbers
+    //print numbers on screen 1 per line
+    //in decimal and haxadecimal(Uppercase) format
+    //right aligned, occupying 6 spaces with a spacing of 2 between them
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insere 4 numeros inteiros:");
+            string n1=Console.ReadLine();
+            string n2=Console.ReadLine();
+            string n3=Console.ReadLine();
+            string n4=Console.ReadLine();
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
